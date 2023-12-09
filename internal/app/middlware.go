9bb6721483bedfaf10d13f84808a1e727b60d52f
Ben Cyrus <bencyrus@Bens-MacBook-Pro.local>
2023-12-08 20:23:48 -0500

    removed request log in the middleware

--- internal/app/middlware.go
+++ internal/app/middlware.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -10,8 +9,6 @@ func CorsControls(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Business-Name")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST")
-
-		fmt.Printf("%v\n", r)
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
